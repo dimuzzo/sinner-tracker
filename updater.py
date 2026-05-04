dc483aeccf82dcf5948476394b0472fdb9f6c763
dimuzzo <ale03dex5@gmail.com>
2026-05-04 15:03:51 +0200

    API fullfilled, while waiting for the restart I've added the new stats.

--- updater.py
+++ updater.py
@@ -97,7 +97,7 @@ def update_database():
                         else:
                             opp_name = match.get('player1', {}).get('name', 'TBD')
                         
-                        t_name = base_match.get('tournament') or f"Madrid Open" 
+                        t_name = base_match.get('tournament') or f"Internazionali d'Italia" 
                         r_name = base_match.get('round') or f"Round {match.get('roundId', 'TBD')}"
                         
                         exact_match_data = {
@@ -118,7 +118,7 @@ def update_database():
         elif base_match:
             db['next_match'] = {
                 "opponent": base_match.get('opponent_name') or "TBD",
-                "tournament": base_match.get('tournament') or "Madrid Open",
+                "tournament": base_match.get('tournament') or "Internazionali d'Italia",
                 "round": base_match.get('round') or "TBD",
                 "date": base_match.get('date') or None
             }
